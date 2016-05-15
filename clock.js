5763a17682891c523a9b13b2ee7903128fd3f209
AlexSunChen <sunchen1221@gmail.com>
2016-05-16 01:26:29 +0900

    testV1
    alarm 機能はまだ実験中

--- clock.js
+++ clock.js
@@ -2,7 +2,7 @@
  * Created by SunChen on 2016/05/12.
  */
 
-
+var alarmTime;
 var setAlarm = function(newtime){
     var thedate;
     if(newtime instanceof Date){
@@ -10,22 +10,53 @@ var setAlarm = function(newtime){
         thedate=newtime;
     }
     else{
-        //convert from string formatted like hh[:mm[:ss]]]
         var arr = newtime.split(':');
         thedate=new Date();
         for(var i= 0; i <3 ; i++){
-            //force to int
             arr[i]=Math.floor(arr[i]);
-            //check if NaN or invalid min/sec
             if( arr[i] !==arr[i] || arr[i] > 59) arr[i]=0 ;
-            //no more than 24h
             if( i==0 && arr[i] > 23) arr[i]=0 ;
         }
         thedate.setHours(arr[0],arr[1],arr[2]);
     }
-    //alert(el.id);
-    el.alarmTime = thedate;
+    alarmTime = thedate;
 };
 
-var synced_delay= 1000 - ((new Date().getTime()) % 1000);
-setTimeout(function(){startClock(x,y);},synced_delay);
+const ISAlarmTime = function (nowTime){
+    var end_min = alarmTime.getHours()*60*60+alarmTime.getMinutes()*60+alarmTime.getSeconds();
+    var now = nowTime.getHours()*60*60+nowTime.getMinutes()*60+nowTime.getSeconds();
+    if (end_min === now)
+    return true ;
+    else return false;
+}
+
+function checkTime() {
+    if(ISAlarmTime(new Date()))
+        alert("time march");
+    setTimeout(function(){checkTime();},1000);
+}
+function LoopTime(){
+    var newDate = new Date();
+    var h = newDate.getHours();
+    var m = newDate.getMinutes();
+    var s = newDate.getSeconds();
+
+    $("#clock").text(h+":"+m+":"+s);
+
+
+    /*if(alarmTime === null){
+        alert("undefined");
+    }*/
+    if (ISAlarmTime)
+
+    var synced_delay = 1000;
+    setTimeout(function(){LoopTime();},synced_delay);
+}
+
+$(document).ready(function () {
+    $("#set").click(function () {
+        setAlarm($("#set").val());
+        checkTime();
+    });
+    LoopTime();
+});
